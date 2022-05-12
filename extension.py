952aec851602b62b879018f5eddab769bd36f382
sarathkumarchintha <105163231+sarathkumarchintha@users.noreply.github.com>
2022-05-12 17:13:22 +0530

    Add files via upload

---
+++ extension.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 17:08:49 2022
+
+@author: sarat
+"""
+
+filename= input("Input filename:")
+f_extens=filename.split(".")
+
+print("extension of file is:"+repr(f_extens[-1]))
