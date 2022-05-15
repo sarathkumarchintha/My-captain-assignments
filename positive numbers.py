0f298633d7f9f89c1d92b3eb5b31e6e5b1afa532
sarathkumarchintha <105163231+sarathkumarchintha@users.noreply.github.com>
2022-05-15 10:18:39 +0530

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,8 @@
+nums = [12,14,-95,3]
+print("original numbers in the list:",nums)
+new_nums = list(filter(lambda x: x >0, nums))
+print("positive numbers in the list: ",new_nums)
+nums = [12,-7,5,64,-14]
+print("original numbers in the list:",nums)
+new_nums = list(filter(lambda x: x >0, nums))
+print("positive numbers in the list: ",new_nums)
